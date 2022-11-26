3f4e3b189d95daf6a30fa4c30ed918ea58efd414
rdouglas <ruairidouglas@gmail.com>
2022-11-26 11:04:13 +0000

    chore: move tracer

--- tracing.js
+++ tracing.js
@@ -1,15 +1,20 @@
 /*tracing.js*/
 // Require dependencies
 const opentelemetry = require("@opentelemetry/sdk-node");
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
-
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
-  instrumentations: [getNodeAutoInstrumentations()]
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: "http://localhost:4317/v1/traces/v1/traces",
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
-
-sdk.start()
+sdk.start();
